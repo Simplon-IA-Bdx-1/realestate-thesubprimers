d10b377414ff0c66a846df3f2f1474359209c128
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2020-03-18 15:07:19 +0100

    + scrapping branch

--- scrap_immo.py
+++ scrap_immo.py
@@ -9,9 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import os
-
-working_dir = "C:/Users/simplon/Desktop/scrappy_immo"
-os.chdir(working_dir)
+import time
 
 # Initialize variables
 ville = []
@@ -25,48 +23,53 @@ typelogement = []
 
 # Create a session (to avoid redirecting loop with requests.get())
 s = requests.Session()
-s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-
 
 # Loop over different pages
-max_pages = 220
-page = 1
+max_pages = 2
+page = 0
 while (page <= max_pages):
-    url = ("https://www.logic-immo.com/vente-immobilier-paris-75,100_1/options/groupprptypesids=1,6,7,12,15")
+    print("hey")
+    url = ("https://www.logic-immo.com/vente-immobilier-paris-75,100_1/options/groupprptypesids=1,6")
     page += 1
     try:
+        print("connection")
         r = s.get(url)
+        # Get information from the page
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        # find all ads from the page
+        annonces = soup.find_all("div", attrs={"class": "offer offer-list"})
+        
+        for annonce in annonces:
+            # get properties of the appartment
+            ville.append(annonce.find("span", attrs={"class": "offer-details-location--locality"}).get_text())
+            if annonce.find("a", attrs={"class": "offer-details-location--city"}):
+                quartier.append(annonce.find("a", attrs={"class": "offer-details-location--city"}).get_text())
+            else:
+                quartier.append("")
+            price.append(annonce.find("p", attrs={"class": "offer-price"}).get_text())
+            typelogement.append(annonce.find("span", attrs={"class": "offer-details-type"}).get_text())
+            #description.append(annonce.find("div", attrs={"class": "Card__Description-sc-7insep-4 ContentZone__ClassifiedDescription-wghbmy-0 fNmvry"}).get_text())
+            # room = annonce.find("span", attrs={"class": "offer-details-caracterististik--rooms"})
+            # room_nb.append(room.find("span", attrs={"class": "offer-rooms-number"}).get_text())
+            # bedroom = ("span", attrs={"class": "offer-details-caracterististik--bedrooms"})
+            bedroom_nb.append(annonce.find("span", attrs={"class": "offer-rooms-number"}).get_text())
+            surface.append(annonce.find("span", attrs={"class": "offer-area-number"}).get_text())
+            print("OK")
+
+        time.sleep(5)
     except:
+        print("raté")
         # Stop visiting pages
         break
     
-    # Get information from the page
-    soup = BeautifulSoup(r.text, 'html.parser')
-    
-    # find all ads from the page
-    annonces = soup.find_all("div", attrs={"class": "offer-list-row"})
-    
-    print(annonces)
-    
-    for annonce in annonces:
-        # get properties of the appartment
-        ville.append(annonce.find("span", attrs={"class": "offer-details-location--locality"}).get_text())
-        quartier.append(annonce.find().get_text())
-        price.append(annonce.find("p", attrs={"class": "offer-price"}).get_text())
-        typelogement.append(annonce.find("span", attrs={"class": "offer-details-type"}).get_text())
-        #description.append(annonce.find("div", attrs={"class": "Card__Description-sc-7insep-4 ContentZone__ClassifiedDescription-wghbmy-0 fNmvry"}).get_text())
-        room = annonce.find("span", attrs={"class": "offer-details-caracterististik--rooms"})
-        room_nb.append(room.find("span", attrs={"class": "offer-rooms-number"}).get_text())
-        bedroom = annonce.find("span", attrs={"class": "offer-details-caracterististik--bedrooms"})
-        bedroom_nb.append(bedroom.find("span", attrs={"class": "offer-rooms-number"}).get_text())
-        surface.append(annonce.find("span", attrs={"class": "offer-area-number"}).get_text())
 
 
 rst = pd.DataFrame({"type of logement": typelogement,
                     "ville": ville,
                     "quartier" : quartier,
                     "price": price,
-                    "room_nb": room_nb,
+                    # "room_nb": room_nb,
                     "bedroom_nb": bedroom_nb,
                     "surface": surface})
 
@@ -75,4 +78,4 @@ new_names = [name + "_raw" for name in new_names]
 rst.columns = new_names
     
 # Write result to csv
-rst.to_csv("immo-paris-page1-to-500_raw.csv", encoding="utf-8")
+rst.to_csv("testos.csv", encoding="utf-8")
--- scrap_seloger.py
+++ scrap_seloger.py
@@ -6,12 +6,10 @@ Script to retrieve data from the site seloger.com
 import pandas as pd
 import numpy as np
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs4
 import re
-import os
-
-working_dir = "C:/Users/utilisateur/Desktop/HousePricesAZ"
-os.chdir(working_dir)
+import time
+import random
 
 # Initialize variables
 ville = []
@@ -29,7 +27,7 @@ s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleW
 
 
 # Loop over different pages
-max_pages = 500
+max_pages = 1
 page = 1
 while (page <= max_pages):
     url = ("http://www.seloger.com/list.htm?org=advanced_search&idtt=2&idtypebien=2,1&cp=75&tri=initial&LISTING-LISTpg=" +
@@ -37,39 +35,41 @@ while (page <= max_pages):
     page += 1
     try:
         r = s.get(url)
+        soup = bs4(r.text, 'html.parser')
+
+        annonces = soup.find_all("div", attrs={"class": "Card__ContentZone-sc-7insep-3 cKGHWc"})
+
+        if len(annonces) > 0:
+            for annonce in annonces:
+                # get properties of the appartment
+                locality_list = (annonce.find("div", attrs={"class": "Card__LabelGap-sc-7insep-5 ContentZone__Address-wghbmy-2 eAcNsS"}).find_all("span"))
+                if (len(locality_list)==2):
+                    ville.append(locality_list[0].get_text())
+                    quartier.append(locality_list[1].get_text())
+                    price.append(annonce.find("div", attrs={"class": "Price__Label-sc-1g9fitq-1 kYWVBR"}).get_text())
+                    typelogement.append(annonce.find("div", attrs={"class": "Card__LabelGap-sc-7insep-5 ContentZone__Title-wghbmy-6 hXERKq"}).get_text())
+                    #description.append(annonce.find("div", attrs={"class": "Card__Description-sc-7insep-4 ContentZone__ClassifiedDescription-wghbmy-0 fNmvry"}).get_text())
+
+                    property_list = annonce.find("ul", attrs={"class": "ContentZone__Tags-wghbmy-7 fNlgGF"}).find_all("li")
+                    if (len(property_list) == 3):
+                        room_nb.append(property_list[0].get_text())
+                        bedroom_nb.append(property_list[1].get_text())
+                        surface.append(property_list[2].get_text())
+                    else:
+                        room_nb.append("")
+                        bedroom_nb.append("")
+                        surface.append("")
+            print("enregistré")
+        else:
+            print("captcha")
+            pass
+
+        time.sleep(random.randrange(5,10))
     except:
         # Stop visiting pages
         break
     
-    # Get information from the page
-    soup = BeautifulSoup(r.text, 'html.parser')
     
-    # find all ads from the page
-    annonces = soup.find_all("div", attrs={"class": "Card__ContentZone-sc-7insep-3 cKGHWc"})
-    print(annonces)
-    
-    for annonce in annonces:
-        # get properties of the appartment
-        locality_list = (annonce.find("div", attrs={"class": "Card__LabelGap-sc-7insep-5 ContentZone__Address-wghbmy-2 eAcNsS"}).find_all("span"))
-        if (len(locality_list)==2):
-            ville.append(locality_list[0].get_text())
-            quartier.append(locality_list[1].get_text())
-        else:
-            ville.append("")
-            quartier.append("")
-        price.append(annonce.find("div", attrs={"class": "Price__Label-sc-1g9fitq-1 kYWVBR"}).get_text())
-        typelogement.append(annonce.find("div", attrs={"class": "Card__LabelGap-sc-7insep-5 ContentZone__Title-wghbmy-6 hXERKq"}).get_text())
-        #description.append(annonce.find("div", attrs={"class": "Card__Description-sc-7insep-4 ContentZone__ClassifiedDescription-wghbmy-0 fNmvry"}).get_text())
-
-        property_list = annonce.find("ul", attrs={"class": "ContentZone__Tags-wghbmy-7 fNlgGF"}).find_all("li")
-        if (len(property_list) == 3):
-            room_nb.append(property_list[0].get_text())
-            bedroom_nb.append(property_list[1].get_text())
-            surface.append(property_list[2].get_text())
-        else:
-            room_nb.append("")
-            bedroom_nb.append("")
-            surface.append("")
 
 rst = pd.DataFrame({"type of logement": typelogement,
                     "ville": ville,
@@ -84,4 +84,4 @@ new_names = [name + "_raw" for name in new_names]
 rst.columns = new_names
     
 # Write result to csv
-rst.to_csv("seloger-paris-page1-to-500_raw.csv", sep=";", encoding="utf-8")
+rst.to_csv("./test_uno.csv", sep=";", encoding="utf-8")
