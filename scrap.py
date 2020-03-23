bf27396b171b7742258e846e5046bd0d0bb093a2
xNlyo <45298194+xNlyo@users.noreply.github.com>
2020-03-23 11:23:49 +0100

    added start and end pages

--- scrap.py
+++ scrap.py
@@ -5,74 +5,71 @@ import pandas as pd
 import time
 import random as rd
 
-try:
+#specify start and end pages
+start = 1 
+end = 100
 
-    nbpages= 100
-    url = "https://www.seloger.com/list.htm?projects=2%2C5&types=1%2C2&natures=1%2C2%2C4&places=%5B%7Bci%3A750056%7D%5D&enterprise=0&qsVersion=1.0&LISTING-LISTpg="
+url = "https://www.seloger.com/list.htm?projects=2%2C5&types=1%2C2&natures=1%2C2%2C4&places=%5B%7Bci%3A750056%7D%5D&enterprise=0&qsVersion=1.0&LISTING-LISTpg="
 
-    option = webdriver.ChromeOptions()
-    option.add_argument(' — incognito')
+option = webdriver.ChromeOptions()
+option.add_argument(' — incognito')
 
-    driver = webdriver.Chrome(executable_path='D:/OneDrive/SIMPLON/Subprimers/Scrap/chromedriver.exe')
+driver = webdriver.Chrome(executable_path='D:/OneDrive/SIMPLON/Subprimers/Scrap/chromedriver.exe')
 
-    driver.maximize_window()
+driver.maximize_window()
 
-    df = pd.DataFrame(columns = ['price', 'rooms', 'bedrooms', 'surface', 'district', 'text', 'plus', 'general', 'inside', 'others'])
+df = pd.DataFrame(columns = ['price', 'rooms', 'bedrooms', 'surface', 'district', 'text', 'plus', 'general', 'inside', 'others'])
 
-    def slp():
+def slp():
 
-        return time.sleep(rd.randint(1,2) + rd.random())
+    return time.sleep(rd.randint(1,2) + rd.random())
 
-    slp()
+slp()
 
-    for n in range(1,nbpages):
+for n in range(start,end):
 
-        driver.get(url + str(n))
+    driver.get(url + str(n))
 
-        adds = driver.find_elements_by_name("classified-link")
-        hrefs = [href for href in [element.get_attribute("href") for element in adds] if href.startswith('https://www.seloger.com/annonces/achat/appartement/')]
+    adds = driver.find_elements_by_name("classified-link")
+    hrefs = [href for href in [element.get_attribute("href") for element in adds] if href.startswith('https://www.seloger.com/annonces/achat/appartement/')]
 
-        for href in hrefs:
+    for href in hrefs:
 
-            print(href)
-            driver.get(href)
+        print(href)
+        driver.get(href)
 
-            try:
+        try:
 
-                price = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[2]').text
-                rooms = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[3]/div[1]/div[2]').text
-                bedrooms = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[3]/div[2]/div[2]').text
-                surface = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[3]/div[3]/div[2]').text
-                district = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[1]/div[2]').text
+            price = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[2]').text
+            rooms = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[3]/div[1]/div[2]').text
+            bedrooms = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[3]/div[2]/div[2]').text
+            surface = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[3]/div[3]/div[2]').text
+            district = driver.find_element_by_xpath('//*[@id="top"]/div[2]/div[2]/div[1]/div[2]').text
 
-                slp()
+            slp()
 
-                driver.find_element_by_xpath('//*[@id="showcase-description"]/div[1]/div/div/div/div[2]').click()
-                text = ' '.join([element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[1]/div/div/div/div[1]').find_elements_by_tag_name('p')])
+            driver.find_element_by_xpath('//*[@id="showcase-description"]/div[1]/div/div/div/div[2]').click()
+            text = ' '.join([element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[1]/div/div/div/div[1]').find_elements_by_tag_name('p')])
 
-                slp()
+            slp()
 
-                driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[2]').click()
-                plus = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[1]/div').find_elements_by_tag_name('figcaption')]
-                general = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[1]/div[1]').find_elements_by_tag_name('li')]
-                inside = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[1]/div[2]').find_elements_by_tag_name('li')]
-                others = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[1]/div[3]').find_elements_by_tag_name('li')]
-                
-                print([price, rooms, bedrooms, surface, district, text, plus, general, inside, others])
-                df = df.append({'price':price, 'rooms':rooms, 'bedrooms':bedrooms, 'surface':surface, 'district':district, 'text':text, 'plus':plus, 'general':general, 'inside':inside, 'others':others}, ignore_index=True)
-                
+            driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[2]').click()
+            plus = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[1]/div').find_elements_by_tag_name('figcaption')]
+            general = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[1]/div[1]').find_elements_by_tag_name('li')]
+            inside = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[1]/div[2]').find_elements_by_tag_name('li')]
+            others = [element.text for element in driver.find_element_by_xpath('//*[@id="showcase-description"]/div[2]/div[2]/div[1]/div[3]').find_elements_by_tag_name('li')]
+            
+            print([price, rooms, bedrooms, surface, district, text, plus, general, inside, others])
+            df = df.append({'price':price, 'rooms':rooms, 'bedrooms':bedrooms, 'surface':surface, 'district':district, 'text':text, 'plus':plus, 'general':general, 'inside':inside, 'others':others}, ignore_index=True)
+            
+            print(n)
 
-                slp()
+            slp()
 
-            except (selenium.common.exceptions.NoSuchElementException, selenium.common.exceptions.ElementClickInterceptedException):
+            df.to_csv('data.csv',sep=';')
 
-                continue
-        
-        n +=1
+        except (selenium.common.exceptions.NoSuchElementException, selenium.common.exceptions.ElementClickInterceptedException):
 
-except KeyboardInterrupt:
-
-    print(df)
-    df.to_csv('./data.csv', sep=';', encoding='utf-8')
-    driver.close()
-    exit()
+            continue
+    
+    n +=1
