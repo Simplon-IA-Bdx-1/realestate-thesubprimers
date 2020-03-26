929c26dea6fa3fecd6e0e9aebcecc0265e51bed9
YohanCaillau <54558085+YohanCaillau@users.noreply.github.com>
2020-03-26 09:05:50 +0100

    update the app on flask

--- app/app.py
+++ app/app.py
@@ -1,18 +1,32 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify, render_template, url_for
 import pickle
-from sklearn.preprocessing import StandardScaler
 from xgboost import XGBRegressor
+from sklearn.preprocessing import StandardScaler
+
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config.from_object('config')
 standard_x = StandardScaler()
 model = pickle.load(open('house_model.pkl', 'rb'))
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    data = pd.read_csv('seloger-cleaned.csv', encoding="utf-8")
+    villes = data['ville_raw'].dropna()
+    villes = villes.sort_values()
+    villes = villes.unique()
+    quartiers = data['quartier_raw'].dropna()
+    quartiers = quartiers.sort_values()
+    quartiers = quartiers.unique()
+    return render_template('index.html', villes = list(villes), quartiers = list(quartiers))
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 @app.route('/predict', methods=['POST'])
@@ -25,9 +39,9 @@ def predict():
     standard_value = standard_x.fit_transform(final_features)
     prediction = model.predict(standard_x.transform(standard_value))
 
-    output = round(prediction[0], 2)
+    output = int(prediction[0])
 
-    return render_template('index.html', prediction_text='Le prix estimée du bien est {} €'.format(output))
+    return render_template('index.html', prediction_text='Le prix estimé du bien est de {} €'.format(output))
 
 
 @app.route('/predict_api', methods=['POST'])
